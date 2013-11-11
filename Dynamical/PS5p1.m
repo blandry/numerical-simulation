03523b5fc9a2410b1bc31d57d282cacd5335f99e
Benoit Landry <landry@mit.edu>
2013-11-11 18:31:33 -0500

    modif to bar model

--- Dynamical/PS5p1.m
+++ Dynamical/PS5p1.m
@@ -6,12 +6,15 @@ dt = 0.001;
 tf = 5;
 
 u1 = zeros(N+1,1);
-u1(N/2) = 0.01;
+u1(N/2,1) = 1;
 u = repmat(u1,1,tf/dt+1);
 
 fhand=@(x,u) fbar(x,u,dx);
 dfhand=@(x,u) dfbar(x,u,dx);
 
 x = traprule(fhand,dfhand,u,x0,tf,dt);
+%x = foreuler(fhand,dfhand,u,x0,tf,dt);
 
-plot(1:501,x(:,1001),1:501,x(:,2001),1:501,x(:,3001),1:501,x(:,4001),1:501,x(:,5001));
+plot(1:501,x(:,1),1:501,x(:,1001),1:501,x(:,2001),1:501,x(:,3001),1:501,x(:,4001),1:501,x(:,5001));
+legend('t=0','t=1','t=2','t=3','t=4','t=5');
+%plot(linspace(0,tf,tf/dt+1),x(250,:));
--- Dynamical/fbar.m
+++ Dynamical/fbar.m
@@ -3,11 +3,11 @@ function [ f ] = fbar( x, u, dx )
 %   Detailed explanation goes here
 
 f=zeros(size(x,1),1);
-f(1,1)=u(1,1)+(x(2,1)-2*x(1,1))/dx^2;
+f(1,1)=u(1,1)+(x(2,1)-2*x(1,1));%/dx^2;
 for i=2:size(f,1)-1
-    f(i,1)=u(i,1)+(x(i+1,1)-2*x(i,1)+x(i-1,1))/dx^2;
+    f(i,1)=u(i,1)+(x(i+1,1)-2*x(i,1)+x(i-1,1));%/dx^2;
 end
-f(end,1)=u(end,1)+(x(end-1,1)-2*x(end,1))/dx^2;
+f(end,1)=u(end,1)+(x(end-1,1)-2*x(end,1));%/dx^2;
 
 end
 
